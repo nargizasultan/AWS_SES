9b5d4b2fa2dadd85eb918493baa70f712e11a3bb
nargiza <sultanmuratova500@gmail.com>
2022-09-29 00:41:46 +0600

    GOOGLE CLOUD EMAIL API

---
+++ src/main/java/com/example/ses/GmailOperations.java
@@ -0,0 +1,63 @@
+package com.example.ses;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import com.google.api.client.util.Base64;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
+
+public class GmailOperations {
+
+    public static Message createMessageWithEmail(MimeMessage email) throws MessagingException, IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        email.writeTo(baos);
+        String encodedEmail = Base64.encodeBase64URLSafeString(baos.toByteArray());
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
+
+    public static MimeMessage createEmail(String to, String from, String subject, String bodyText) throws MessagingException, IOException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        MimeMessage email = new MimeMessage(session);
+
+        email.setFrom(new InternetAddress(from)); //me
+        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to)); //
+        email.setSubject(subject);
+
+        email.setText(bodyText);
+
+        return email;
+    }
+
+    public static void sendEmail() throws IOException, GeneralSecurityException, MessagingException {
+
+        Gmail service = GmailAPI.getGmailService();
+        MimeMessage Mimemessage = createEmail("@gmail.com","@gmail.com","Hello ","This is my body text");
+
+        Message message = createMessageWithEmail(Mimemessage);
+
+        message = service.users().messages().send("me", message).execute();
+
+        System.out.println("Message id: " + message.getId());
+        System.out.println(message.toPrettyString());
+    }
+
+
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException, MessagingException {
+
+        sendEmail();
+
+    }
+
+
+}
